ae57047e50e7d71076c16e495b866c550ea942c2
Lunear01 <IkerHuang0315@gmail.com>
2026-03-21 22:15:20 -0400

    Refined header files

--- include/client.h
+++ include/client.h
@@ -1,14 +1,20 @@
-void connect_to_server()
+#include "server.h"
 
+typedef enum {
+     STATUS_OFFLINE, 
+     STATUS_ONLINE, 
+     STATUS_DND 
+} UserStatus;
 
-void create_server()
-void leave_server()
-void create_channel()
-void delete_channel()
+void connect_to_server(const char *ip_address, int port);
 
-void send_friend_request()
-void accept_friend_request()
+void create_server(const char *server_name);
+void leave_server(int server_id);
 
-void update_user_status(ServerContext *ctx, int client_index, UserStatus new_status); //Default to online if logged in, offline if logged out
+void create_channel(int server_id, const char *channel_name);
+void delete_channel(int channel_id);
 
+void send_friend_request(const char *target_username);
+void accept_friend_request(const char *target_username);
 
+void update_user_status(ServerContext *ctx, int client_index, UserStatus new_status);
--- include/protocol.h
+++ include/protocol.h
@@ -1,29 +1,66 @@
 // This is the file that both server and client will include
-// Prevents segfaults when packets being sent/received 
+// Prevents segfaults when packets being sent/received
+
 typedef enum {
     MSG_LOGIN,
     MSG_DM,
     MSG_SERVER,
     MSG_CHANNEL,
-    MSG_BROADCAST // For Testing
 } PacketType;
 
-typedef struct {
+// --- Action Flags ---
+// These help the server know exactly what to do with the packet
 
+typedef enum {
+    AUTH_LOGIN,
+    AUTH_REGISTER,
+    AUTH_LOGOUT
+} AuthAction;
+
+typedef enum {
+    DM_MESSAGE,
+    DM_FRIEND_REQUEST,
+    DM_FRIEND_ACCEPT
+} DMAction;
+
+typedef enum {
+    SERVER_CREATE,
+    SERVER_JOIN,
+    SERVER_LEAVE
+} ServerAction;
+
+typedef enum {
+    CHANNEL_CREATE,
+    CHANNEL_DELETE,
+    CHANNEL_JOIN
+} ChannelAction;
+
+// --- Payloads ---
+
+typedef struct {
+    AuthAction action;
+    char username[32];
+    char password[64]; // In a real app, ensure this is hashed!
 } AuthPayload;
 
-
 typedef struct {
-
+    DMAction action;
+    char message[512]; // The actual text content
 } DMPayload;
 
 typedef struct {
-
+    ServerAction action;
+    int server_id;
+    char server_name[32];
 } ServerPayload;
 
 typedef struct {
-
+    ChannelAction action;
+    int server_id;
+    int channel_id;
+    char channel_name[32];
 } ChannelPayload;
+
 
 typedef struct {
     PacketType type;
@@ -33,7 +70,7 @@ typedef struct {
         DMPayload dm;
         ServerPayload server;
         ChannelPayload channel;
-    }
+    } payload; 
     long timestamp;
     char receiver[32];
 } TizcordPacket;
