33303073c80849cecb55e870389b3ca6caf2b140
yibo <yibo@DESKTOP-PF11EF6.localdomain>
2026-04-01 21:54:07 -0400

    remoed all junk from social header

--- server/include/tizcord_social.h
+++ server/include/tizcord_social.h
@@ -4,12 +4,6 @@
 #define SOCIAL_H
 #include "server.h"
 
-void send_friend_request(ServerContext* ctx, int sender_index, const char* recipient_username);
-void accept_friend_request(ServerContext* ctx, int recipient_index, const char* sender_username);
-void list_friends(ServerContext* ctx, int client_index);
-void remove_friend(ServerContext* ctx, int client_index, const char* friend_username);
-void check_online_users(ServerContext* ctx, int client_index);
-void update_user_status(ServerContext* ctx, int client_index, UserStatus new_status);
 void handle_social_packet(ServerContext *ctx, ClientNode *client, TizcordPacket *packet);
 
 #endif
